076d66993cfce1344da2d48cd005e01d0707170b
Ubuntu <ubuntu@ip-172-31-18-217.eu-west-1.compute.internal>
2019-06-07 21:54:44 +0000

    working https!

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"crypto/tls"
+	//"crypto/tls"
 	"log"
 	goHttp "net/http"
 	"time"
 
 	"github.com/kennedymj97/todo-api/http"
 	"github.com/kennedymj97/todo-api/postgres"
-	"golang.org/x/crypto/acme/autocert"
+	//"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -27,23 +27,23 @@ func main() {
 	userHandler.UserService = dbClient.UserService()
 
 	// Create new server
-	certManager := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("api.mattkennedy.io"),
-		Cache:      autocert.DirCache("certs"),
-	}
+	//certManager := autocert.Manager{
+	//	Prompt:     autocert.AcceptTOS,
+	//	HostPolicy: autocert.HostWhitelist("api.mattkennedy.io"),
+	//	Cache:      autocert.DirCache("certs"),
+	//}
 
 	s := &goHttp.Server{
 		Addr:         ":https",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
-		TLSConfig: &tls.Config{
-			GetCertificate: certManager.GetCertificate,
-		},
+		//TLSConfig: &tls.Config{
+		//	GetCertificate: certManager.GetCertificate,
+		//},
 	}
 	s.Handler = &http.Handler{TaskHandler: taskHandler, UserHandler: userHandler}
 
-	go goHttp.ListenAndServe(":http", certManager.HTTPHandler(nil))
+	//go goHttp.ListenAndServe(":http", certManager.HTTPHandler(nil))
 
-	log.Fatal(s.ListenAndServeTLS("", ""))
+	log.Fatal(s.ListenAndServeTLS("/etc/letsencrypt/live/api.mattkennedy.io/fullchain.pem", "/etc/letsencrypt/live/api.mattkennedy.io/privkey.pem"))
 }
